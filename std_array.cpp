44337dbe3418491a840bac9581e40d154188f345
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-20 18:23:30 -0700

    terminal chaeck for errors

--- std_array.cpp
+++ std_array.cpp
@@ -3,6 +3,21 @@
 #include <iostream>
 #include <array>
 #include <string>
-
+using namespace std;
 //Declare const for values that should not change
-const int daysInWeek = 7;
+const int SIZE = 7;
+
+int main()
+{
+//declarations
+array<string, SIZE >pillSchedule = {
+        "Vitamin",   // Sunday (index 0)
+        "None",      // Monday (index 1)
+        "Medication",// Tuesday (index 2)
+        "None",      // Wednesday (index 3)
+        "Vitamin",   // Thursday (index 4)
+        "Medication",// Friday (index 5)
+        "None"  };
+        cout<< "1. SIZE:  "<<pillSchedule.size()<<endl;
+    return 0;
+}

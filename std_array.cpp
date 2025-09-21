1cb9837399e2f5d0a36f2612fd9e039127b2f6dc
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-20 18:00:36 -0700

    starting std array

---
+++ std_array.cpp
@@ -0,0 +1,8 @@
+//comsc 210|Lateefah Camacho| Lab 12
+// vs code
+#include <iostream>
+#include <array>
+#include <string>
+
+//Declare const for values that should not change
+const int daysInWeek = 7;
